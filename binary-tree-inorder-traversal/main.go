ca3aa9220717a89899bb567cde33031bac19abe1
Igor Ivanov <[email protected]>
2024-03-26 23:58:57 +0700

    task №94 has been solved alternatively

--- binary-tree-inorder-traversal/main.go
+++ binary-tree-inorder-traversal/main.go
@@ -6,13 +6,36 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
+//func inorderTraversal(root *TreeNode) []int {
+//	if root == nil {
+//		return []int{}
+//	}
+//	var l []int
+//	l = append(l, inorderTraversal(root.Left)...)
+//	l = append(l, root.Val)
+//	l = append(l, inorderTraversal(root.Right)...)
+//	return l
+//}
+
 func inorderTraversal(root *TreeNode) []int {
 	if root == nil {
 		return []int{}
 	}
-	var l []int
-	l = append(l, inorderTraversal(root.Left)...)
-	l = append(l, root.Val)
-	l = append(l, inorderTraversal(root.Right)...)
-	return l
+
+	deque := make([]*TreeNode, 0)
+
+	nodes := []int{}
+	for root != nil || len(deque) != 0 {
+		for root != nil {
+			deque = append(deque, root)
+			root = root.Left
+		}
+		lastNode := len(deque) - 1
+		root = deque[lastNode]
+		deque = deque[:lastNode]
+		nodes = append(nodes, root.Val)
+		root = root.Right
+	}
+
+	return nodes
 }
