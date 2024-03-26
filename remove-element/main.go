6c0f8a75ae8281123a5353a1d713f1ebd6898c8d
Igor Ivanov <[email protected]>
2024-03-26 22:11:30 +0700

    task №27 has been solved

---
+++ remove-element/main.go
@@ -0,0 +1,16 @@
+package remove_element
+
+// https://leetcode.com/problems/remove-element/description/
+
+func removeElement(nums []int, val int) int {
+	k := 0
+
+	for i := 0; i < len(nums); i++ {
+		if nums[i] != val {
+			nums[k] = nums[i]
+			k++
+		}
+	}
+
+	return k
+}
